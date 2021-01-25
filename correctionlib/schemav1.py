ee63b519a34a94a63fc563baa82aacb8bb6a67ba
Nick Smith <nick.smith@cern.ch>
2021-01-25 09:56:49 -0600

    Support python 3.6+

--- correctionlib/schemav1.py
+++ correctionlib/schemav1.py
@@ -2,7 +2,6 @@ from typing import (
     List,
     Optional,
     Union,
-    ForwardRef,
     Literal,
 )
 from pydantic import BaseModel
@@ -21,6 +20,7 @@ class Variable(Model):
     type: Literal["string", "int", "real"]
     "Implicitly 64 bit integer and double-precision floating point?"
     description: Optional[str]
+    # TODO: clamping behavior for out of range?
 
 
 class Formula(Model):
@@ -30,11 +30,10 @@ class Formula(Model):
     "Index to Correction.inputs[]"
 
 
+# None = invalid phase space?
 Value = Union[Formula, float]
-Binning = ForwardRef("Binning")
-MultiBinning = ForwardRef("MultiBinning")
-Category = ForwardRef("Category")
-Content = Union[Binning, MultiBinning, Category, Value]
+# py3.7+: ForwardRef can be used instead of strings
+Content = Union["Binning", "MultiBinning", "Category", Value]
 
 
 class Binning(Model):
