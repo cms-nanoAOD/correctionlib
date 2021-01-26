a439a9f502639f2a0a4209e40c569401e9953576
Nick Smith <nick.smith@cern.ch>
2021-01-26 01:11:05 -0600

    Make python take *args

--- demo.py
+++ demo.py
@@ -4,5 +4,6 @@ cset = libcorrection.CorrectionSet("data/examples.json")
 for corr in cset:
     print(corr.name())
 
-out = cset["DeepCSV_2016LegacySF"].evaluate(["central", 0, 1.2, 35., 0.01]);
+sf = cset["DeepCSV_2016LegacySF"]
+out = sf.evaluate("central", 0, 1.2, 35., 0.01)
 print(out)
--- src/python.cc
+++ src/python.cc
@@ -9,16 +9,31 @@ PYBIND11_MODULE(libcorrection, m) {
 
     py::class_<Correction>(m, "Correction")
         .def("name", &Correction::name)
-        .def("evaluate", &Correction::evaluate);
-        // .def("evaluate", [](Correction& c, py::args args) {
-        //   return c.evaluate(*args);
-        // });
+        .def("evaluate", [](Correction& c, py::args args) {
+          std::vector<Variable::Type> varargs;
+          for (auto handle : args) {
+            if ( py::isinstance<py::int_>(handle) ) {
+              varargs.push_back(py::cast<int>(handle));
+            }
+            else if ( py::isinstance<py::float_>(handle) ) {
+              varargs.push_back(py::cast<double>(handle));
+            }
+            else if ( py::isinstance<py::str>(handle) ) {
+              varargs.push_back(py::cast<std::string>(handle));
+            }
+            else {
+              throw std::runtime_error("Cannot interpret argument");
+            }
+          }
+          return c.evaluate(varargs);
+        }); 
 
     py::class_<CorrectionSet>(m, "CorrectionSet")
         .def(py::init<const std::string &>())
         .def("__getitem__", &CorrectionSet::operator[])
         .def("__len__", &CorrectionSet::size)
         .def("__iter__", [](const CorrectionSet &v) {
-            return py::make_iterator(v.begin(), v.end());
+          // FIXME: this is not a mapping
+          return py::make_iterator(v.begin(), v.end());
         }, py::keep_alive<0, 1>());
 }
