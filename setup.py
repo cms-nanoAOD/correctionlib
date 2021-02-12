5729f5e493312f93ea2d0804acee37b4a470962e
Nick Smith <nick.smith@cern.ch>
2021-02-12 16:28:01 -0600

    Platform-dependent compile args

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/nsmith-/correctionlib for details.
 
+import sys
+
 from setuptools import setup  # isort:skip
 
 # Available at setup time due to pyproject.toml
@@ -13,6 +15,11 @@ from pybind11.setup_helpers import Pybind11Extension  # isort:skip
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
+if sys.platform.startswith("win"):
+    extra_compile_args = ["/Zc:__cplusplus", "/O2"]
+else:
+    extra_compile_args = ["-O3"]
+
 ext_modules = [
     Pybind11Extension(
         "correctionlib._core",
@@ -20,8 +27,7 @@ ext_modules = [
         cxx_std=17,
         include_pybind11=False,
         include_dirs=["rapidjson/include", "pybind11/include", "cpp-peglib", "include"],
-        extra_compile_args=["-O3", "/Zc:__cplusplus"],
-        # TODO: Windows build needs /Zc:__cplusplus
+        extra_compile_args=extra_compile_args,
     ),
 ]
 
