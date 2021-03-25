cd6f9ea632255bf30d3d4616ce2adc67945598a8
Pieter David <pieter.david@cern.ch>
2021-03-25 16:58:46 +0100

    Fix formatting

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/nsmith-/correctionlib for details.
 
-from skbuild import setup
 from setuptools import find_packages
 from setuptools_scm import get_version
+from skbuild import setup
 
-setup(packages=find_packages(where="src"), package_dir={"": "src"}, cmake_install_dir="src",
-        cmake_args=["-DCORRECTIONLIB_VERSION={0}".format(".".join(get_version().split(".")[:2]))])
+setup(
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    cmake_install_dir="src",
+    cmake_args=[
+        "-DCORRECTIONLIB_VERSION={}".format(".".join(get_version().split(".")[:2]))
+    ],
+)
