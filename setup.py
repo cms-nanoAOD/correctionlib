03bd4a224999ecb5023faf77955dadc4e2ba2ea5
Pieter David <pieter.david@cern.ch>
2021-03-25 16:54:46 +0100

    fix a typo

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ from setuptools import find_packages
 from setuptools_scm import get_version
 
 setup(packages=find_packages(where="src"), package_dir={"": "src"}, cmake_install_dir="src",
-        cmake_args-["-DCORRECTIONLIB_VERSION={0}".format(".".join(get_version().split(".")[:2]))])
+        cmake_args=["-DCORRECTIONLIB_VERSION={0}".format(".".join(get_version().split(".")[:2]))])
