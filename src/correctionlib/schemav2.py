148e625a2e50a588bfe8b9bd6c5fdd53da8777b2
Nick Smith <nick.smith@cern.ch>
2021-03-25 12:36:38 -0500

    Add some Category validation

--- src/correctionlib/schemav2.py
+++ src/correctionlib/schemav2.py
@@ -172,6 +172,20 @@ class Category(Model):
     content: List[CategoryItem]
     default: Optional[Content]
 
+    @validator("content")
+    def validate_content(cls, content: List[CategoryItem]) -> List[CategoryItem]:
+        if len(content):
+            keytype = type(content[0].key)
+            if not all(isinstance(item.key, keytype) for item in content):
+                raise ValueError(
+                    f"Keys in the Category node do not have a homogenous type, expected all {keytype}"
+                )
+
+            keys = {item.key for item in content}
+            if len(keys) != len(content):
+                raise ValueError("Duplicate keys detected in Category node")
+        return content
+
 
 Transform.update_forward_refs()
 Binning.update_forward_refs()
--- tests/test_issue058.py
+++ tests/test_issue058.py
@@ -14,3 +14,25 @@ def test_categoryitem_coercion():
         x = schema.CategoryItem(key=b"30", value=1.0)
     x = schema.CategoryItem(key="30xyz", value=1.0)
     assert x.key == "30xyz"
+
+
+def test_cat_valid():
+    with pytest.raises(ValueError):
+        schema.Category(
+            nodetype="category",
+            input="x",
+            content=[
+                schema.CategoryItem(key="30xyz", value=1.0),
+                schema.CategoryItem(key=30, value=1.0),
+            ],
+        )
+
+    with pytest.raises(ValueError):
+        schema.Category(
+            nodetype="category",
+            input="x",
+            content=[
+                schema.CategoryItem(key="30xyz", value=1.0),
+                schema.CategoryItem(key="30xyz", value=1.0),
+            ],
+        )
