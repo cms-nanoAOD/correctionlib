60f758749b3ea13b074fc23b4ec4a98618e5c367
Pieter David <pieter.david@cern.ch>
2021-03-25 16:34:28 +0100

    Pass major.minor version to cmake

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@
 
 from skbuild import setup
 from setuptools import find_packages
-setup(packages=find_packages(where="src"), package_dir={"": "src"}, cmake_install_dir="src")
+from setuptools_scm import get_version
+
+setup(packages=find_packages(where="src"), package_dir={"": "src"}, cmake_install_dir="src",
+        cmake_args-["-DCORRECTIONLIB_VERSION={0}".format(".".join(get_version().split(".")[:2]))])
