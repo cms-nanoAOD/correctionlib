fbcc030844177415047e580832e907877261dfeb
Pieter David <pieter.david@cern.ch>
2021-03-26 12:49:38 +0100

    remove unnecessary os import

--- src/correctionlib/__init__.py
+++ src/correctionlib/__init__.py
@@ -6,7 +6,6 @@ import sys
 
 if sys.platform.startswith("win32"):
     import ctypes
-    import os
     import os.path
 
     ctypes.CDLL(os.path.join(os.path.dirname(__file__), "lib", "correctionlib.dll"))
