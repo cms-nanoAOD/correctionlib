9972bf526732cf8d7b528eaac97d1234bb4b2bd0
Pieter David <pieter.david@cern.ch>
2021-03-26 11:18:05 +0100

    explicit DLL loading on windows, first try

--- src/correctionlib/__init__.py
+++ src/correctionlib/__init__.py
@@ -1,3 +1,14 @@
 from .version import version as __version__
 
 __all__ = ("__version__",)
+
+import sys
+if sys.platform.startswith("win32"):
+    import ctypes
+    import pkg_resources
+    import os, os.path
+    try:
+        ctypes.CDLL(pkg_resources.resource_filename(__name__, os.path.join("lib", "correctionlib.dll")))
+    except Exception as ex:
+        print(f'Failed to load {pkg_resources.resource_filename(__name__, os.path.join("lib", "correctionlib.dll"))}')
+        print(f'Contents of "correctionlib/lib": {os.listdir(pkg_resources.resource_filename("correctionlib", "lib"))}')
