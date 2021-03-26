07353bd1a6e63c488a2ce733699160e8ae2d1c4d
Pieter David <pieter.david@cern.ch>
2021-03-26 12:11:52 +0100

    black; more ls

--- src/correctionlib/__init__.py
+++ src/correctionlib/__init__.py
@@ -3,11 +3,22 @@ from .version import version as __version__
 __all__ = ("__version__",)
 
 import sys
+
 if sys.platform.startswith("win32"):
     import ctypes
-    import os, os.path
+    import os
+    import os.path
+
     try:
         ctypes.CDLL(os.path.join(os.path.dirname(__file__), "lib", "correctionlib.dll"))
     except Exception as ex:
-        print(f'Failed to load {os.path.join(os.path.dirname(__file__), "lib", "correctionlib.dll")}')
-        print(f'Contents of "correctionlib/lib": {os.listdir(os.path.join(os.path.dirname(__file__), "lib"))}')
+        print(
+            f'Failed to load {os.path.join(os.path.dirname(__file__), "lib", "correctionlib.dll")}'
+        )
+        print(f'Contents of "correctionlib": {os.listdir(os.path.dirname(__file__))}')
+        print(
+            f'Contents of "correctionlib/..": {os.listdir(os.path.dirname(os.path.dirname(__file__)))}'
+        )
+        print(
+            f'Contents of "correctionlib/lib": {os.listdir(os.path.join(os.path.dirname(__file__), "lib"))}'
+        )
