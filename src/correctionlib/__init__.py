aded63a6b2e81d35e7e7f040d7f29efe59725157
Pieter David <pieter.david@cern.ch>
2021-03-26 12:03:27 +0100

    relative path with __file__ instead of pkg_resources

--- src/correctionlib/__init__.py
+++ src/correctionlib/__init__.py
@@ -5,10 +5,9 @@ __all__ = ("__version__",)
 import sys
 if sys.platform.startswith("win32"):
     import ctypes
-    import pkg_resources
     import os, os.path
     try:
-        ctypes.CDLL(pkg_resources.resource_filename(__name__, os.path.join("lib", "correctionlib.dll")))
+        ctypes.CDLL(os.path.join(os.path.dirname(__file__), "lib", "correctionlib.dll"))
     except Exception as ex:
-        print(f'Failed to load {pkg_resources.resource_filename(__name__, os.path.join("lib", "correctionlib.dll"))}')
-        print(f'Contents of "correctionlib/lib": {os.listdir(pkg_resources.resource_filename("correctionlib", "lib"))}')
+        print(f'Failed to load {os.path.join(os.path.dirname(__file__), "lib", "correctionlib.dll")}')
+        print(f'Contents of "correctionlib/lib": {os.listdir(os.path.join(os.path.dirname(__file__), "lib"))}')
