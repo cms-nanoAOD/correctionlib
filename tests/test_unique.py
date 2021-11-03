8ac665227da15211b4851a7bd321e4b2f1dc0218
Nicholas Smith <nick.smith@cern.ch>
2021-11-03 14:22:00 -0500

    Add a check for unique names for items in CorrectionSet (#109)

---
+++ tests/test_unique.py
@@ -0,0 +1,48 @@
+import pytest
+
+import correctionlib.schemav2
+
+
+def test_unique():
+    def make(name):
+        return {
+            "name": name,
+            "description": "",
+            "inputs": [],
+            "output": {"name": "out", "type": "real"},
+            "version": 1,
+            "data": 1.1,
+        }
+
+    def makec(name):
+        return {
+            "name": name,
+            "output": {"name": "out", "type": "real"},
+            "inputs": [],
+            "inputs_update": [],
+            "input_op": "*",
+            "output_op": "last",
+            "stack": [],
+        }
+
+    correctionlib.schemav2.CorrectionSet.parse_obj(
+        {
+            "schema_version": 2,
+            "corrections": [make("thing1"), make("thing2")],
+            "compound_corrections": [makec("thing1"), makec("thing2")],
+        }
+    )
+
+    with pytest.raises(ValueError):
+        correctionlib.schemav2.CorrectionSet.parse_obj(
+            {"schema_version": 2, "corrections": [make("thing1"), make("thing1")]}
+        )
+
+    with pytest.raises(ValueError):
+        correctionlib.schemav2.CorrectionSet.parse_obj(
+            {
+                "schema_version": 2,
+                "corrections": [make("thing1"), make("thing1")],
+                "compound_corrections": [makec("thing"), makec("thing")],
+            }
+        )
