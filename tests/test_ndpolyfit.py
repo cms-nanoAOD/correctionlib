61e6e7df130b3439dd0ffc71ee9b785682ffa7bd
Nick Smith <nick.smith@cern.ch>
2022-09-08 11:40:26 -0500

    Fix test

--- tests/test_ndpolyfit.py
+++ tests/test_ndpolyfit.py
@@ -5,7 +5,7 @@ from correctionlib import convert
 
 
 def test_ndpoly():
-    corr = convert.ndpolyfit(
+    corr, _ = convert.ndpolyfit(
         points=[np.array([0.0, 1.0, 0.0, 1.0]), np.array([10.0, 20.0, 10.0, 20.0])],
         values=np.array([0.9, 0.95, 0.94, 0.98]),
         weights=np.array([0.1, 0.1, 0.1, 0.1]),
