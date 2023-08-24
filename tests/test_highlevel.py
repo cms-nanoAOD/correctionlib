3c2b8f442284f6e1f227e4d38be7379228d6ef9b
Nick Smith <nick.smith@cern.ch>
2023-08-24 11:06:00 -0500

    Refine highlevel tests for awkward1

--- tests/test_highlevel.py
+++ tests/test_highlevel.py
@@ -8,8 +8,9 @@ import correctionlib
 from correctionlib import schemav2 as model
 
 
-def test_highlevel():
-    cset = correctionlib.CorrectionSet(
+@pytest.fixture
+def cset():
+    return correctionlib.CorrectionSet(
         model.CorrectionSet(
             schema_version=model.VERSION,
             corrections=[
@@ -26,6 +27,9 @@ def test_highlevel():
             ],
         )
     )
+
+
+def test_highlevel(cset):
     assert set(cset) == {"test corr"}
     sf = cset["test corr"]
     assert sf.version == 2
@@ -47,6 +51,25 @@ def test_highlevel():
     sf2 = pickle.loads(pickle.dumps(sf))
     assert sf2.evaluate(1.0, 1.0) == 1.234
 
+
+def test_highlevel_flatawkward(cset):
+    sf = cset["test corr"]
+    numpy.testing.assert_array_equal(
+        sf.evaluate(awkward.Array(numpy.ones((3, 4))), numpy.ones(4)),
+        numpy.full((3, 4), 1.234),
+    )
+    numpy.testing.assert_array_equal(
+        sf.evaluate(numpy.ones((3, 4)), awkward.Array(numpy.ones(4))),
+        numpy.full((3, 4), 1.234),
+    )
+
+
+@pytest.mark.skipif(
+    awkward.__version__.startswith("1."),
+    reason="Complex awkward array support only for ak v2+",
+)
+def test_highlevel_awkward(cset):
+    sf = cset["test corr"]
     numpy.testing.assert_array_equal(
         awkward.flatten(sf.evaluate(awkward.unflatten(numpy.ones(6), [3, 2, 1]), 1.0)),
         numpy.full(6, 1.234),
@@ -65,24 +88,7 @@ def test_highlevel():
     )
 
 
-def test_highlevel_dask():
-    cset = correctionlib.CorrectionSet(
-        model.CorrectionSet(
-            schema_version=model.VERSION,
-            corrections=[
-                model.Correction(
-                    name="test corr",
-                    version=2,
-                    inputs=[
-                        model.Variable(name="a", type="real"),
-                        model.Variable(name="b", type="real"),
-                    ],
-                    output=model.Variable(name="a scale", type="real"),
-                    data=1.234,
-                )
-            ],
-        )
-    )
+def test_highlevel_dask(cset):
     sf = cset["test corr"]
 
     dask_awkward = pytest.importorskip("dask_awkward")
