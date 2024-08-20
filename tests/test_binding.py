1d83fff93c8495f44a1628caec45c275bc9c2a93
Nick Smith <nick.smith@cern.ch>
2024-08-20 18:03:17 -0500

    test in ci...

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import shutil
 import subprocess
@@ -88,5 +89,7 @@ def test_cmake_static_compilation(csetstr: str):
             print(ret.stdout.decode())
             print(ret.stderr.decode())
             raise RuntimeError(f"cmake build failed (args: {ret.args})")
+        for f in glob.glob(os.path.join(tmpdir, "*")):
+            print(f)
         prog = "test.exe" if os.name == "nt" else "test"
         subprocess.run([os.path.join(tmpdir, prog)], check=True)
