5de3fc6f8f049fe585fa342e518625994a9e458c
Nick Smith <nick.smith@cern.ch>
2025-11-25 15:48:18 -0600

    more test

--- tests/test_issue250.py
+++ tests/test_issue250.py
@@ -1,4 +1,5 @@
 import correctionlib.schemav2 as cs
+from correctionlib.highlevel import Correction
 
 
 def test_issue250():
@@ -41,3 +42,39 @@ def test_issue250():
     assert corr.evaluate(20.0) == 0.8434999999999999
     assert corr.evaluate(25.0) == 0.8434999999999999
     assert corr.evaluate(35.0) == 0.0
+
+
+def _make(formula: str) -> Correction:
+    formula_obj = cs.Formula(
+        nodetype="formula",
+        expression=formula,
+        parser="TFormula",
+        variables=["x"],
+    )
+    return cs.Correction(
+        name="test",
+        version=2,
+        inputs=[cs.Variable(name="x", type="real")],
+        output=cs.Variable(name="a scale", type="real"),
+        data=formula_obj,
+    ).to_evaluator()
+
+
+def test_formula_logic():
+    assert _make("x > 5").evaluate(6.0) == 1.0
+    assert _make("x > 5").evaluate(5.0) == 0.0
+
+    assert _make("x < 5").evaluate(4.0) == 1.0
+    assert _make("x < 5").evaluate(5.0) == 0.0
+
+    assert _make("x > 5 || x < 3").evaluate(6.0) == 1.0
+    assert _make("x > 5 || x < 3").evaluate(4.0) == 0.0
+    assert _make("x > 5 || x < 3").evaluate(2.0) == 1.0
+
+    assert _make("x > 5 && x < 10").evaluate(11.0) == 0.0
+    assert _make("x > 5 && x < 10").evaluate(6.0) == 1.0
+    assert _make("x > 5 && x < 10").evaluate(4.0) == 0.0
+
+    assert _make("(x > 5 && x < 10) || (x > 20 && x < 30)").evaluate(6.0) == 1.0
+    assert _make("(x > 5 && x < 10) || (x > 20 && x < 30)").evaluate(25.0) == 1.0
+    assert _make("(x > 5 && x < 10) || (x > 20 && x < 30)").evaluate(15.0) == 0.0
